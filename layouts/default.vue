bc7cf248e1110778da7f5ba5daa2bcede8990eff
Alosies George <alosies@gmail.com>
2021-09-14 11:24:13 +0530

    Initial commit with stylus typescript and NCAPI

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <header>Header</header>
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+header {
+  height: 10rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ reactiveData }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    const reactiveData = ref('this is composed')
+
+    return {
+      reactiveData,
+    }
+  },
+})
+</script>
+
+<style lang="stylus" scoped></style>
